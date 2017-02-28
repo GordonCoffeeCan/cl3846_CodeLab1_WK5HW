e749b21b20e15471dca783b68c66af65ad24d15a
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-02-27 23:07:48 -0500

    set project week5

---
+++ Code_Lab_Week5_HW/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+	public float speed = 5;
+	public float rotationSpeed = 8;
+    public Transform _weaponPivot;
+    public Animator pistolAnim;
+
+    public static Vector3 playerPosition;
+
+    private Transform _mainCamera;
+
+	private Vector3 _moveDirection;
+	private float _gravity = 20;
+	private Transform _transfrom;
+    private AudioSource pistolShootAuido;
+
+    private Ray _ray;
+
+	private CharacterController _characterController;
+
+	// Use this for initialization
+	void Start () {
+		_mainCamera = Camera.main.transform;
+		_transfrom = this.transform;
+		_moveDirection = Vector3.zero;
+		_characterController = this.GetComponent<CharacterController> ();
+        pistolShootAuido = this.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        pistolAnim.SetBool("isFire", false);
+        if (Input.GetMouseButtonDown(0)) {
+            pistolAnim.SetBool("isFire", true);
+            pistolShootAuido.Play();
+
+            
+
+            _ray = new Ray(_mainCamera.position, _mainCamera.forward);
+            RaycastHit _rayHit = new RaycastHit();
+
+            if (Physics.Raycast(_ray, out _rayHit)) {
+                if(_rayHit.collider.tag == "Target") {
+                    Destroy(_rayHit.collider.gameObject);
+                    TargetSpawner.isTargetSpawned = false;
+                    ScoreManager.instance.Score++;
+                }
+            }
+        }
+
+        playerPosition = this.transform.position;
+
+    }
+
+	void FixedUpdate(){
+        Movement();
+        FPSCamera();
+        WeaponRotate();
+        _moveDirection.y -= _gravity * Time.deltaTime;
+		_characterController.Move (_moveDirection * Time.deltaTime);
+		_mainCamera.position = new Vector3(_transfrom.position.x, _transfrom.position.y + 1, _transfrom.position.z);
+    }
+
+    private void FPSCamera() {
+        _mainCamera.Rotate(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        //Set Limitation of Camera Rotation;
+        if(_mainCamera.localEulerAngles.x > 75 && _mainCamera.localEulerAngles.x < 180) {
+            _mainCamera.localEulerAngles = new Vector3(75, _mainCamera.localEulerAngles.y, 0);
+        }else if (_mainCamera.localEulerAngles.x > 180 && _mainCamera.localEulerAngles.x < 285) {
+            _mainCamera.localEulerAngles = new Vector3(285, _mainCamera.localEulerAngles.y, 0);
+        } else {
+            _mainCamera.localEulerAngles = new Vector3(_mainCamera.localEulerAngles.x, _mainCamera.localEulerAngles.y, 0);
+        }
+    }
+
+    private void WeaponRotate() {
+        Quaternion _controllerTargetRotation = Quaternion.Euler(0, _mainCamera.eulerAngles.y, 0);
+        Quaternion _pivotTargetRotation = Quaternion.Euler(_mainCamera.eulerAngles.x, _mainCamera.eulerAngles.y, 0);
+        _transfrom.rotation = Quaternion.Slerp(_transfrom.rotation, _controllerTargetRotation, rotationSpeed * Time.deltaTime);
+        _weaponPivot.rotation = Quaternion.Slerp(_weaponPivot.rotation, _pivotTargetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void Movement() {
+        if (_characterController.isGrounded) {
+            _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            _moveDirection = _transfrom.TransformDirection(_moveDirection);
+            _moveDirection *= speed;
+        }
+    }
+}
---
+++ Code_Lab_Week5_HW/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    public float offSetX = 0;
+    public float offSetY = 0;
+
+    public string[] fileNames;
+    public static int levelNum;
+
+	// Use this for initialization
+	void Start () {
+        string fileName = fileNames[levelNum];
+
+        string filePath = Application.dataPath + "/" + fileName;
+
+        StreamReader sr = new StreamReader(filePath);
+
+        GameObject levelHolder = new GameObject("Level Holder");
+
+        int yPos = 0;
+
+        while (!sr.EndOfStream) {
+            string line = sr.ReadLine();
+
+            for(int xPos = 0; xPos < line.Length; xPos++) {
+                if(line[xPos] == 'x') {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                    cube.transform.parent = levelHolder.transform;
+
+                    cube.transform.position = new Vector3(xPos + offSetX, yPos + offSetY, 0);
+
+                    if(line[xPos] == 'p') {
+                        GameObject player = Instantiate(Resources.Load("Prefabs/Balloon") as GameObject);
+                    }
+                }
+            }
+
+            yPos--;
+
+            
+        }
+        sr.Close();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            levelNum++;
+            SceneManager.LoadScene("Week5");
+        }
+	}
+}
