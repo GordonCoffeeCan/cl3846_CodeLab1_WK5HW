18b498170c5b66485346a7498d39409567c05bea
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-03-01 12:52:23 -0500

    new files for next week

---
+++ Code_Lab_Week5_HW/Assets/Scripts/UtilScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class UtilScript : MonoBehaviour {
+
+	public static void WriteStringToFile(string path, string fileName, string content) {
+        StreamWriter _sw = new StreamWriter(path + "/" + fileName);
+
+        _sw.WriteLine(content);
+
+        _sw.Close();
+
+    }
+}
